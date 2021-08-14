ca29268417748e698ec80013abf43c1d537b8d45
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-08-14 20:17:08 +0800

    useCallback > custom hooks : make safeDispatch with useCallback, useRef, and useEffect

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -10,6 +10,23 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
+function useSafeDispatch(dispatch) {
+  const mountedRef = React.useRef(false)
+
+  React.useLayoutEffect(() => {
+    mountedRef.current = true
+    return () => {
+      mountedRef.current = false
+    }
+  }, [])
+
+  return React.useCallback((...args) => {
+    if (mountedRef.current) {
+      dispatch(...args)
+    }
+  }, [dispatch])
+}
+
 // 🐨 this is going to be our generic asyncReducer
 function asyncReducer(state, action) {
   switch (action.type) {
@@ -29,12 +46,14 @@ function asyncReducer(state, action) {
 }
 
 function useAsync(initialState) {
-  const [state, dispatch] = React.useReducer(asyncReducer, {
+  const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
     error: null,
     ...initialState
   })
+
+  const dispatch = useSafeDispatch(unsafeDispatch)
 
   const run = React.useCallback(promise => {
     dispatch({ type: 'pending' })
@@ -46,7 +65,7 @@ function useAsync(initialState) {
         dispatch({ type: 'rejected', error })
       },
     )
-  }, [])
+  }, [dispatch])
 
   return {...state, run}
 }
